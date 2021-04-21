5e1229785ee40bfa6d508b08d7bdec4b7fc3b436
whykhamist <jhydenellana@gmail.com>
2021-04-21 16:59:35 +0800

    Setup copy classes

---
+++ Kopya/FileCopyProgressInfo.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace Kopya
+{
+    public class FileCopyProgressInfo : ICopyProgressInfo
+    {
+        /// <summary>
+        /// The file name of the file to being copied.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// The filename including path currently being copied.
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The Progress (in percentage value) of the copy task.
+        /// </summary>
+        public double Progress { get; set; }
+
+        /// <summary>
+        /// Check if operation has been cancelled
+        /// </summary>
+        public bool Cancelled { get; set; } = false;
+
+        /// <summary>
+        /// The total file size of the file being copied.
+        /// </summary>
+        public long FileSize { get; set; }
+
+        /// <summary>
+        /// The Total size copied from the file.
+        /// </summary>
+        public long FileSizeCopied { get; set; }
+    }
+}
---
+++ Kopya/ICopyProgressInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kopya
+{
+    public interface ICopyProgressInfo
+    {
+        string FileName { get; set; }
+
+        string FullName { get; set; }
+
+        double Progress { get; set; }
+
+        bool Cancelled { get; set; }
+    }
+}
